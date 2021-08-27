26fd91b00280f6a3800730dd109dd94bbe9c1ec9
Jeff Biseda <jbiseda@gmail.com>
2021-08-27 05:47:19 -0700

    x

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -507,20 +507,18 @@ fn get_rpc_node(
             let mut eligible_rpc_peers = vec![];
 
             for rpc_peer in rpc_peers.iter() {
-                /*
                 if no_untrusted_rpc
                     && !is_trusted_validator(&rpc_peer.id, &validator_config.trusted_validators)
                 {
-                    error!("validator/main get_rpc_node no untrusted");
+                    //error!("validator/main get_rpc_node no untrusted");
                     continue;
                 }
-                */
                 cluster_info.get_snapshot_hash_for_node(&rpc_peer.id, |snapshot_hashes| {
                     for snapshot_hash in snapshot_hashes {
                         if let Some(ref trusted_snapshot_hashes) = trusted_snapshot_hashes {
                             if !trusted_snapshot_hashes.contains(snapshot_hash) {
                                 // Ignore all untrusted snapshot hashes
-                                error!("validator/main get_rpc_node no trusted hashes");
+                                //error!("validator/main get_rpc_node no trusted hashes");
                                 continue;
                             }
                         }
@@ -540,7 +538,7 @@ fn get_rpc_node(
                 });
             }
 
-            error!("validator/main get_rpc_node after rpc_peers scan");
+            //error!("validator/main get_rpc_node after rpc_peers scan");
 
             match highest_snapshot_hash {
                 None => {
