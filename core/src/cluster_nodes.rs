00c2ee792de354aaa6afcd4711748da76dabc4d0
Jeff Biseda <jbiseda@gmail.com>
2022-02-28 02:25:22 -0800

    32

--- core/src/cluster_nodes.rs
+++ core/src/cluster_nodes.rs
@@ -423,7 +423,7 @@ impl ClusterNodes<RetransmitStage> {
             thread_pool.borrow().install(|| {
                 indices
                     .into_par_iter()
-                    .with_min_len(16)
+                    .with_min_len(32)
                     .map(|index| {
                         let shred_stakes = self
                             .get_deterministic_shred_distribution_stakes_by_slot_and_index(
