843d6be87739083ba65969ffec88945cacf28a31
Jeff Biseda <jbiseda@gmail.com>
2022-03-14 23:44:52 -0700

    x

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -3614,8 +3614,8 @@ impl Blockstore {
 
         let mut completed_slots = self.completed_unrepaired_slots.lock().unwrap();
 
-        let min_completed = *completed_slots.iter().min().unwrap();
-        let max_completed = *completed_slots.iter().max().unwrap();
+        let min_completed = *completed_slots.iter().min().unwrap_or(0);
+        let max_completed = *completed_slots.iter().max().unwrap_or(0);
 
         let min_slot = slots.iter().min().unwrap();
 
