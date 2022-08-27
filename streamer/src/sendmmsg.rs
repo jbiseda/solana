3aac0c023e39a8a4bd611c6cfc8f0997546590a4
Jeff Biseda <jbiseda@gmail.com>
2022-08-26 18:06:05 -0700

    x

--- streamer/src/sendmmsg.rs
+++ streamer/src/sendmmsg.rs
@@ -138,8 +138,9 @@ where
     let mut addrs = vec![unsafe { std::mem::zeroed() }; size];
     let mut hdrs = vec![unsafe { std::mem::zeroed() }; size];
     for ((pkt, dest), hdr, iov, addr) in izip!(packets, &mut hdrs, &mut iovs, &mut addrs) {
-        if pkt.len() != 1232 {
-            error!(">>> pkt.len={}", pkt.len());
+        let pkt_len = pkt.as_ref().len();
+        if pkt_len != 1232 {
+            error!(">>> pkt.len={}", pkt_len);
         }
         mmsghdr_for_packet(pkt.as_ref(), dest.borrow(), iov, addr, hdr);
     }
